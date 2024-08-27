220d7a7ee28bb9ac5d0f21666bfe09551ecc1d79
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-08-27 17:50:32 -0500

    Feat: Test of the method update in Client completed

--- src/test/java/com/riwi/beautySalon/infraestructure/services/ClientServiceTest.java
+++ src/test/java/com/riwi/beautySalon/infraestructure/services/ClientServiceTest.java
@@ -90,4 +90,53 @@ public class ClientServiceTest {
         // Then
         verify(clientRepository).delete(clientEntity);
     };
+
+    // Actualizar
+    @Test
+    public void testUpdate() {
+        
+        // Given
+        Long clientId = 1L;
+
+        ClientEntity existingClient = new ClientEntity();
+        existingClient.setId(clientId);
+        existingClient.setFirstName("Juan");
+        existingClient.setLastName("Gomez");
+        existingClient.setEmail("juan@gmail.com");
+        existingClient.setPhone("987654321");
+        existingClient.setAppointments(DataProviderClient.appointmentsEntity());
+
+        ClientReq clientReq = new ClientReq();
+        clientReq.setFirstName("Pepe");
+        clientReq.setLastName("Perez");
+        clientReq.setEmail("pepe@gmail.com");
+        clientReq.setPhone("123484844");
+
+        ClientEntity updatedClientEntity = new ClientEntity();
+        updatedClientEntity.setId(clientId);
+        updatedClientEntity.setFirstName("Pepe");
+        updatedClientEntity.setLastName("Perez");
+        updatedClientEntity.setEmail("pepe@gmail.com");
+        updatedClientEntity.setPhone("123484844");
+        updatedClientEntity.setAppointments(existingClient.getAppointments());
+
+        when(this.clientRepository.findById(clientId)).thenReturn(Optional.of(existingClient));
+        when(this.clientRepository.save(any(ClientEntity.class))).thenReturn(updatedClientEntity);
+
+        // When
+        ClientResp response = clientService.update(clientReq, clientId);
+
+        // Then
+        assertNotNull(response);
+        assertEquals(clientId, response.getId());
+        assertEquals("Pepe", response.getFirstName());
+        assertEquals("Perez", response.getLastName());
+        assertEquals("123484844", response.getPhone());
+        assertEquals("pepe@gmail.com", response.getEmail());
+        assertEquals(existingClient.getAppointments().size(), response.getAppointments().size());
+
+        verify(clientRepository).save(any(ClientEntity.class));
+        verify(clientRepository).findById(clientId);
+    };
+
 };
